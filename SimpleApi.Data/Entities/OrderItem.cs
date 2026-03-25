08f62274d074eb70ec08e44d25cf9fa5ba4886e6
kamneklogs <kamneklogs@gmail.com>
2026-03-25 18:56:43 -0500

    Some changes

--- SimpleApi.Data/Entities/OrderItem.cs
+++ SimpleApi.Data/Entities/OrderItem.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace SimpleApi.Data.Entities;
 
-public class Order
+public class OrderItem
 {
     public int Id { get; set; }
+    public int OrderId { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
 }
--- SimpleApi.Data/Entities/Product.cs
+++ SimpleApi.Data/Entities/Product.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿
 
 namespace SimpleApi.Data.Entities;
 
@@ -9,12 +8,4 @@ public class Product
     public string Name { get; set; } = string.Empty;
     public decimal Price { get; set; }
     public int Stock { get; set; }
-}
-
-internal class ProductConfiguration : IEntityTypeConfiguration<Product>
-{
-    public void Configure(EntityTypeBuilder<Product> builder)
-    {
-        builder.HasKey(o => o.Id);
-    }
 }
